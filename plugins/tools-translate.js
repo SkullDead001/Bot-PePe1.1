20c839f7ca0c0a98c41dcd8c69c742a2c168b3a8
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-09 00:05:27 -0400

    Update tools-translate.js

--- plugins/tools-translate.js
+++ plugins/tools-translate.js
@@ -3,41 +3,43 @@ const defaultLang = 'es'
 const tld = 'cn'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-
-let msg = `📌 Ejemplo :
+    let err = `
+📌 Ejemplo :
 *${usedPrefix + command}* <idioma> [texto]
 ${usedPrefix + command} es Hello World
 
 ✳️ Lista de idiomas admitidos: 
 
-https://cloud.google.com/translate/docs/languages`
+https://cloud.google.com/translate/docs/languages
+`.trim()
 
-if (!args || !args[0]) return m.reply(msg)
-let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) {
-lang = defaultLang
-text = args.join(' ') }
-if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-let result
-try {
-result = await translate(`${text}`, {
-tld,
-to: lang, })
-} catch (e) {
-result = await translate(`${text}`, {
-tld,
-to: defaultLang, })
-} finally {
-m.reply(result[0])
-   }
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    let result
+    try {
+        result = await translate(`${text}`, {
+            tld,
+            to: lang,
+        })
+    } catch (e) {
+        result = await translate(`${text}`, {
+            tld,
+            to: defaultLang,
+        })
+        throw err
+    } finally {
+        m.reply(result[0])
+    }
+
 }
-
-
 handler.help = ['trad <leng> <text>']
 handler.tags = ['tools']
 handler.command = ['translate', 'tl', 'trad', 'tr']
 
-
 export default handler
-
