af2316e7b41a0e2d16d459940abf2649705426a2
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-08 23:19:10 -0400

    Create rpg-unreg.js

---
+++ plugins/rpg-unreg.js
@@ -0,0 +1,17 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { args, usedPrefix}) {
+  if (!args[0]) throw `✳️ *Ingrese número de serie*\nVerifique su número de serie con el comando...\n\n*${usedPrefix}nserie*`
+  let user = global.db.data.users[m.sender]
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  if (args[0] !== sn) throw '⚠️ *Número de serie incorrecto*'
+  user.registered = false
+  m.reply(`✅ Registro eliminado`)
+}
+handler.help = ['unreg <Num Serie>'] 
+handler.tags = ['exp']
+
+handler.command = ['unreg'] 
+handler.register = true
+
+export default handler
+
