a50153a5f1c544834963e992f15bc09b8ebe7528
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-14 00:05:27 -0400

    Update nsfw-cosplay.js

--- plugins/nsfw-cosplay.js
+++ plugins/nsfw-cosplay.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
 var {age} = db.data.users[m.sender]
    if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
-let cosp = (await axios.get(`https://raw.githubusercontent.com/dylux-fg/img-fg/main/random/pack.json`)).data 
+let cosp = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/pack.json`)).data 
 let url = cosp[Math.floor(Math.random() * cosp.length)]
 conn.sendButton(m.chat, `✅ Resultado 🤭`, `Vea más fotos aquí \nhttps://t.me/+8SKOTyja8rBlYTlh \n\n`, url, [['▷▷ Siguiente', `${usedPrefix + command}`]], m)
 }
