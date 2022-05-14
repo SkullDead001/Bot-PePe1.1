321edb00cc369de40c38eb77de7c7a988ff3c475
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-13 23:59:42 -0400

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 	
-	let img = (await axios.get(`https://raw.githubusercontent.com/dylux-fg/img-fg/main/random/loli.json`)).data
+	let img = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/loli.json`)).data
   
 await conn.sendButton(m.chat, '✅ Aquí tienes', 'Click en siguiente para ir a la siguiente imagen', pickRandom(img), [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
 }
