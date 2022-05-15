f605c4bf08d0c82822470973c8852d56aea82815
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-15 17:03:30 -0400

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,4 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 export async function before(m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
@@ -12,10 +12,14 @@ export async function before(m, { isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.sendButton(m.chat, `*▢ Enlace de grupo detectado* ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, '© FG98 - DyLux', ['Desactivar AntiLink', '/disable antilink'], m)
-        if (isBotAdmin && bot.restrict) {
+        await conn.sendButton(m.chat, `*≡ Enlace Detectado*
+            
+No permitimos enlaces de otros grupos 
+lo siento *${await this.getName(m.sender)}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)
+        if (isBotAdmin && chat.antiLink) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('Owner deshabitar kick automático')
+        } else if (!chat.antiLink) return //m.reply('')
     }
     return !0
+    
 }
