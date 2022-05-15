4755dca52c9bf5b2b77f66288c17edf4506d08cc
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-15 15:59:06 -0400

    Fix

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,9 +1,21 @@
+
+let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
+
 let handler = async (m, { conn }) => {
     if (!m.quoted) throw 'where\'s message?'
     if (m.quoted.mtype !== 'viewOnceMessage') throw '✳️ Eso no es un mensaje de viewOnce'
-    const msg = await conn.loadMessage(m.quoted.id)
-    if (!msg) throw '✳️ no puedo abrir el mensaje'
-    await conn.copyNForward(m.chat, msg, true, { readViewOnce: true })
+    let msg = m.quoted.message
+    let type = Object.keys(msg)[0]
+    let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+    let buffer = Buffer.from([])
+    for await (const chunk of media) {
+        buffer = Buffer.concat([buffer, chunk])
+    }
+    if (/video/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+    } else if (/image/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+    }
 }
 
 handler.help = ['readvo']
