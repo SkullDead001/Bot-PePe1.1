2e7629089eae0b7a66192cb98908ad1c71c53aa9
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-15 21:57:26 -0400

    Create main-script.js

---
+++ plugins/main-script.js
@@ -0,0 +1,10 @@
+let handler =  m => m.reply(`
+*≡ SCRIPT*
+
+▢ Git : https://github.com/FG98F/dylux-fg
+`.trim())
+handler.help = ['script']
+handler.tags = ['main']
+handler.command = ['sc', 'git', 'script'] 
+
+export default handler
