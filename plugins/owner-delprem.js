8024c4f1657e79db3edf48fc62d8f7bd23841bcc
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-18 10:41:14 -0400

    Create owner-delprem.js

---
+++ plugins/owner-delprem.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, text }) => {
+
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+    else who = m.chat
+    if (!who) throw `✳️ Menciona al usuario con @`
+    if (!global.prems.includes(who.split`@`[0])) throw '✳️ El usuario no es usuario Premiun'
+    let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
+    global.prems.splice(index, 1)
+    conn.reply(m.chat, `✅ Premium removido \n\n@${who.split('@')[0]} Ya no eres premium`, m, {
+        contextInfo: {
+            mentionedJid: [who]
+        }
+    })
+
+}
+handler.help = ['delprem @user']
+handler.tags = ['owner']
+handler.command = ['delprem', 'delpremium'] 
+handler.group = true
+handler.rowner = true
+
+export default handler
