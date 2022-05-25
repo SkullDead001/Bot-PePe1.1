bf05659597dfa001392217ccd52c20dbce6aacd1
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-25 19:06:30 -0400

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -20,7 +20,7 @@ let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
 let stiker = await sticker(false, res.url, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
+conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
 }}
 
 handler.help = ['emojimix <emoji+emoji>']
