f8d79dfae00c2d5787a69f4f8a122b9603e15c13
FG98 <81941690+FG98F@users.noreply.github.com>
2022-06-05 15:11:12 -0400

    Update sticker-trigger.js

--- plugins/sticker-trigger.js
+++ plugins/sticker-trigger.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 	
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
-    avatar: await conn.profilePictureUrl(who).catch(_ => 'https://i.ibb.co/PZNv21q/Profile-FG98.jpg'),
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
   })
 let stiker = await sticker(false, marah, global.packname, global.author)
   if (stiker) return await conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
